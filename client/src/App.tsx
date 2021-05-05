7b3f8a2015b37ac22c364739689b92d559b6d2e9
Tomas Ellis <tomasitoellis@gmail.com>
2021-05-04 22:47:15 -0300

    Add landing route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import {Route} from 'react-router-dom';
 import Home from '../src/components/home/home';
+import Landing from './components/landing/landing';
 
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="">
+      <Route exact path='/' component={Landing}></Route>
       <Route path='/home' component={Home}></Route>
     </div>
   );
