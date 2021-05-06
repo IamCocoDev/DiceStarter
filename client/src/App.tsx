fc3311d14a68c66043a383913fbd5b7ecdba7c6f
Xaldersoul <benjamin.g.guerra@hotmail.com>
2021-05-06 14:54:06 -0300

    start controled formulary and coneccion with redux

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,8 +4,6 @@ import Home from '../src/components/home/home';
 import Landing from './components/landing/landing';
 import NavBar from '../src/components/NavBar/NavBar';
 import ProductDetail from '../src/components/productDetail/productDetail';
-
-
 import './App.css';
 import About from './components/about/about';
 
--- client/src/types.ts
+++ client/src/types.ts
@@ -1,17 +1,39 @@
-export type Form = {
-    name: string,
-    price: number,
-    categories: string[]
-}
-
 export interface FormState {
-    form: Form;
+    form: Inputs;
     status: 'idle' | 'failed' | 'loading';
 }
 
 export type ToDelete = {
     id: number,
     status: string,
+};
+
+export interface Categories {
+    value: string,
+    label: string
+};
+export interface Inputs {
+    name: string;
+    price: number;
+    categories: Categories[];
+    colors: string[];
+    size: string;
+    stock: number;
+    rating: number;
+    description: string;
+    picture: string;
+}
+
+export interface errorsInput {
+    name: string;
+    price: string;
+    categories: string;
+    colors: string;
+    size: string;
+    stock: string;
+    rating: string;
+    description: string;
+    picture: string;
 }
 
 export type formData = React.FormEvent<HTMLElement>;
