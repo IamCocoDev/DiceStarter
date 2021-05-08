4226d543b33858f7a0027ce715e2b626f34fb134
Angelo <angeloo.esc@gmail.com>
2021-05-08 19:49:06 -0300

    Admin creator & testing relations user/roles

--- api/routes/user.js
+++ api/routes/user.js
@@ -5,10 +5,10 @@ const router = express.Router();
 
 const bcrypt = require('bcrypt');
 
-const { User } = require('../db');
+const { User, Role } = require('../db');
 
 router.get('/:id', (req, res, next) => {
-  User.findByPk(req.params.id)
+  User.findByPk(req.params.id, { include: Role })
     .then((response) => {
       res.json(response);
     }).catch((e) => {
@@ -52,15 +52,53 @@ router.post('/', async (req, res, next) => {
   }
 });
 
-router.put('/:id', (req, res, next) => {
+router.post('/admin', async (req, res, next) => {
+  const id = uuidv4();
+
+  try {
+    let { password } = req.body;
+    const {
+      name,
+      firstName,
+      lastName,
+      birthday,
+      country,
+      email,
+    } = req.body;
+    bcrypt.hash(password, 10, (err, hash) => {
+      password = hash;
+      if (err) {
+        next(err);
+      }
+      const newUser = {
+        id,
+        name,
+        firstName,
+        lastName,
+        birthday,
+        country,
+        email,
+        password,
+      };
+      User.create(newUser).then((info) => { info.addRole(3); res.send(info); })
+        .catch((error) => next(error));
+    });
+  } catch (e) {
+    next(e);
+  }
+});
+
+router.put('/:id', async (req, res, next) => {
   const { id } = req.params;
   const { body } = req;
-  User.update(body, { where: { id } })
-    .then((result) => {
-      res.json(result);
-    }).catch((e) => {
-      next(e);
-    });
+  try {
+    const user = await User.findByPk(id, { include: Role });
+    await user.update(body, { where: { id }, include: Role });
+    user.addRole(body.roles);
+    res.send(user);
+  } catch (err) {
+    next(err);
+  }
 });
 
 module.exports = router;
