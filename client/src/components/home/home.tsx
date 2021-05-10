3e68e328e779fb47d1336546f9820192dccdeb06
Federico Giovenco <giovencofede@gmail.com>
2021-05-10 02:48:37 -0300

    modify v1

--- client/src/components/home/home.tsx
+++ client/src/components/home/home.tsx
@@ -1,5 +1,4 @@
 import React, {useEffect} from 'react';
-import './home.css';
 import ProductCards from '../productCards/productCards';
 import {useAppDispatch, useAppSelector} from '../../app/hooks';
 import {getProductsAsync,
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,17 +1,19 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import {store} from './app/store';
 import {Provider} from 'react-redux';
+import {BrowserRouter} from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-    <React.StrictMode>
-      <Provider store={store}>
+    <Provider store={store}>
+      <BrowserRouter>
         <App />
-      </Provider>
-    </React.StrictMode>,
+      </BrowserRouter>
+    </Provider>,
     document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
