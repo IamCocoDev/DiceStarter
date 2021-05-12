890dfa705d3c49b393baf480edf37ba117ef24a5
Angelo <angeloo.esc@gmail.com>
2021-05-12 12:58:28 -0300

    Update models2

--- api/routes/order.js
+++ api/routes/order.js
@@ -1,68 +1,56 @@
 const express = require('express');
 
-const { v4: uuidv4 } = require('uuid');
-
 const router = express.Router();
 
-const { Order } = require('../db');
+const {
+  User, Order, Productsxorders, Product,
+} = require('../db.js');
 
-router.get('/', (req, res, next) => {
-  Order.findAll()
-    .then((response) => {
-      res.json(response);
-    }).catch((e) => {
-      next(e);
+router.get('/status/:status', (req, res) => {
+  const { status } = req.params;
+  if (status === 'allorders') {
+    Order.findAll({ include: User }).then((data) => res.send(data));
+  } else {
+    Order.findAll({ where: { status }, include: User }).then((result) => {
+      res.send(result);
     });
-});
-
-router.post('/', async (req, res, next) => {
-  const id = uuidv4();
-  const status = 'Active';
-
-  try {
-    const {
-      number,
-      orderDate,
-      fulfilled,
-      paid,
-      paymentDate,
-      shippingAddress,
-      shippingCountry,
-      shippingCity,
-      shippingPostalCode,
-      price,
-      quantity,
-    } = req.body;
-
-    const newProduct = {
-      id,
-      number,
-      orderDate,
-      fulfilled,
-      paid,
-      paymentDate,
-      shippingAddress,
-      shippingCountry,
-      shippingCity,
-      shippingPostalCode,
-      price,
-      quantity,
-      status,
-    };
-    const info = await Order.create(newProduct);
-    res.json(info);
-  } catch (e) {
-    next(e);
   }
 });
 
-router.get('/:id', (req, res, next) => {
-  Order.findByPk(req.params.id)
-    .then((response) => {
-      res.json(response);
-    }).catch((e) => {
-      next(e);
-    });
+router.get('/id/:idOrder', (req, res) => {
+  const { idOrder } = req.params;
+  Order.findAll({ where: { id: idOrder } }).then((data) => {
+    res.send(data);
+  })
+    .catch(() => { res.status(404).send('ERROR'); });
 });
 
+// GET A LAS ORDENES QUE TENGAN ESE PRODUCTO
+router.get('/ORDD/:idProd', (req, res) => {
+  const { idProd } = req.params;
+  Productsxorders.findAll({ where: { product_id: idProd } })
+    .then((data) => {
+      res.send(data);
+    }).catch((error) => res.send(error));
+});
+
+// GET A LAS ORDENES QUE TENGAN ESE PRODUCTO
+router.get('/products/:idOrder', (req, res) => {
+  const { idOrder } = req.params;
+  Order.findOne({ where: { id: idOrder }, include: Product })
+    .then((data) => {
+      res.send(data);
+    }).catch((error) => res.send(error));
+});
+
+router.delete('/orderdelete/:orderId/:productId', (req, res) => {
+  const { orderId, productId } = req.params;
+  Productsxorders.destroy({
+    where: {
+      order_id: orderId, product_id: productId,
+    },
+  })
+    .then(() => res.sendStatus(200))
+    .catch((err) => res.send(err));
+});
 module.exports = router;
