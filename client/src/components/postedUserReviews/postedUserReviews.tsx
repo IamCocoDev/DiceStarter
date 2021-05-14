fdba0cb5b932eb3ec5bd0bef5757317c43d5c4df
Mateo Hernandez <73136132+Mate444@users.noreply.github.com>
2021-05-14 11:38:09 -0300

    rating now displayable

--- client/src/components/postedUserReviews/postedUserReviews.tsx
+++ client/src/components/postedUserReviews/postedUserReviews.tsx
@@ -6,27 +6,14 @@ import {ReviewRes} from '../../types';
 
 const PostedUserReviews = () => {
   const postedReviews = useAppSelector(reviewsResponse);
-  const starsArr = [];
   return (
     <div>
       {
-        postedReviews !== null && postedReviews.map((review: ReviewRes) => {
-          for (let i:number = 0; i < review.rating; i++) {
-            starsArr.push(i);
-            '';
-          }
-          console.log(starsArr);
-          return (
-            <div className='postedReviewsAll' key={review.id}>
-              <p>{review.comment}</p>
-              {
-                starsArr.map((star) => (
-                  <p className='postedReviewsStar' key={star}>★</p>
-                ))
-              }
-            </div>
-          );
-        })
+        postedReviews !== null && postedReviews.map((review: ReviewRes) => (
+          <div className='postedReviewsAll' key={review.id}>
+            <p>{review.comment}</p>
+          </div>
+        ))
       }
     </div>
   );
