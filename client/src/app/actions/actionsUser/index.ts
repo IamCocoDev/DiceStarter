9bbdcbf5d6972699a4859f575266029d6b48b62a
Xaldersoul <benjamin.g.guerra@hotmail.com>
2021-05-17 00:56:53 -0300

    añadido funcion para guardar el inicio de sesion en el local storage, ocurren bugs al añadir objetos al carrito con una sesion de google

--- client/src/app/actions/actionsUser/index.ts
+++ client/src/app/actions/actionsUser/index.ts
@@ -50,6 +50,7 @@ const loginFormAsync = (form: any) => {
     try {
       const res = await axios.post(`http://localhost:3001/user/signin`, form);
       const loginUser = res.data;
+      localStorage.setItem('user', JSON.stringify(loginUser));
       dispatch(sendFormSuccess);
       dispatch(setUser(loginUser));
     } catch (err) {
@@ -63,6 +64,7 @@ const loginGoogle = (user: any) => {
     dispatch(sendFormBegin);
     try {
       dispatch(sendFormSuccess);
+      localStorage.setItem('user', JSON.stringify(user));
       dispatch(setUser(user));
     } catch (err) {
       dispatch(sendFormFailure(err));
--- client/src/components/administratorHome/administratorHome.tsx
+++ client/src/components/administratorHome/administratorHome.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import './administratorHome.css';
-import {useAppSelector} from '../../app/hooks';
-import {userInfo} from '../../app/reducers/registerReducer';
 
 const AdministratorHome = () => {
-  const User = useAppSelector(userInfo);
+  const User = JSON.parse(localStorage.getItem('user') || '{}');
   return (
     <div className='administratorHomeAll'>
-      {User.user ?
-        <h1 className='administratorHomeH1'> Welcome {User.user.name}</h1> :
+      {User.name ?
+        <h1 className='administratorHomeH1'> Welcome {User.name}</h1> :
         <h3 className='administratorHomeH3'>Login please</h3>}
       <h3 className='administratorHomeH3'>
            We expect you to enjoy your experience in DiceStarter!
