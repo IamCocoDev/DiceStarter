de531eed70ee35dfcece5922f9afd34e8c089d20
Mateo Hernandez <73136132+Mate444@users.noreply.github.com>
2021-05-17 02:58:34 -0300

    some styles got fixed

--- client/src/components/home/home.tsx
+++ client/src/components/home/home.tsx
@@ -39,7 +39,7 @@ function Home(props: any) {
       {
         asyncProducts === 'loading' &&
        <div className='homeLoading'>
-         loading products...
+         Loading...
        </div>
       }
       <div>
