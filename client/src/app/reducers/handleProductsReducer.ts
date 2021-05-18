9515a24c24457f81337977679513c682d8743d1d
Mateo Hernandez <73136132+Mate444@users.noreply.github.com>
2021-05-18 10:17:49 -0300

    redux hotfix finished

--- client/src/app/reducers/handleProductsReducer.ts
+++ client/src/app/reducers/handleProductsReducer.ts
@@ -14,7 +14,7 @@ const initialState: Products = {/* Acá definanse un Type en types.ts*/
   productById: null,
   productCategories: [{label: '', value: 0}],
   totalPages: 0,
-  queryFilter: '',
+  queryFilter: 'All',
   querySort: '',
   queryName: '',
 };
