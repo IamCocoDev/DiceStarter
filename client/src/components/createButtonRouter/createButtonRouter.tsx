d864218e3dbce09498ad4f0391bb5b1c4980f5b9
Federico Giovenco <giovencofede@gmail.com>
2021-05-24 09:13:38 -0300

    Changes color in routes

--- client/src/components/createButtonRouter/createButtonRouter.tsx
+++ client/src/components/createButtonRouter/createButtonRouter.tsx
@@ -9,11 +9,11 @@ function CreateButtonRouter() {
     <div className='createButtonRouterGrid'>
       <NavLink to='/create/product' className='createButtonRouterProduct'>
         <button className='createButtonRouterProductButton'>
-          Go to create new product </button>
+          New Product </button>
       </NavLink>
       <NavLink to='/create/category' className='createButtonRouterCategory'>
         <button className='createButtonRouterCategoryButton'>
-          Go to create new category</button>
+          New Category</button>
       </NavLink>
     </div> : <div>401 Not Authorized</div>
   );
