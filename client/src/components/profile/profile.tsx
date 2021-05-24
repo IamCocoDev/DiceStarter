7adc1ed8b53212281e171b84ed7c3dcb8cc6e390
Martín Caime <martincaime@gmail.com>
2021-05-23 23:27:23 -0300

    more and more changes

--- client/src/components/profile/profile.tsx
+++ client/src/components/profile/profile.tsx
@@ -5,6 +5,7 @@ import {logout} from '../../app/actions/actionsUser';
 import {userInfo} from '../../app/reducers/registerReducer';
 import './profile.css';
 import Login from '../login/login';
+import user from '../../img/user.png';
 
 const Profile = () => {
   const dispatch = useAppDispatch();
@@ -16,21 +17,26 @@ const Profile = () => {
     e.preventDefault();
     dispatch(logout());
   };
-  console.log(User);
   return (
     User.name ?
     <div className='profileAll'>
-      <img className='profilePhoto' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQOTxj1L5J64I_-gs_u-l3-d5PKg_wManLB0w&usqp=CAU' alt={User.profilePicture}/>
-      <h3 className='profileName'>{User.firstName} {User.lastName}</h3>
-      <h4 className='profileUsername'>{User.name}</h4>
-      <p className='profileBirthday'>Birthday: <p>{birthDate}</p></p>
-      <p className='profileAddress'> Address:<p contentEditable={editMode}> {User.address}</p> </p>
-      <p className='profileCityLabel'>City: <p contentEditable={editMode}> {User.city}, {User.country}</p></p>
-      <p className='profilePostalLabel'>Postal Code: <p contentEditable={editMode}>{User.postalCode}</p> </p>
-      <p className='profilePhoneNumberLabel'>Phone Number: <p contentEditable={editMode}>{User.phoneNumber}</p> </p>
-      <p className='profileEmailLabel'>Email Adress: <p contentEditable={editMode}> {User.email}</p></p>
-      <input className='profileEditButton' type='button' value='Edit' onClick={() => setEditMode(!editMode)}/>
-      <input className='profileLogOut' type='button' value='Log Out' onClick={handleLogout}/>
+      <div className='profileLeft'>
+        <img className='profilePhoto' src={User.profilePicture || user} alt='profile Pic'/>
+        <h3 className='profileName'>{User.firstName} {User.lastName}</h3>
+        <h4 className='profileUsername'>{User.name}</h4>
+      </div>
+      <div className='profileRight'>
+        <p className='profileBirthday'>Birthday: <p className= 'noteditable'>{birthDate}</p></p>
+        <p className='profileAddress'> Address:<p className={editMode ? 'editable' : 'noteditable'} contentEditable={editMode}> {User.address}</p> </p>
+        <p className='profileCity'>City: <p className={editMode ? 'editable' : 'noteditable'} contentEditable={editMode}> {User.city}, {User.country}</p></p>
+        <p className='profilePostal'>Postal Code: <p className={editMode ? 'editable' : 'noteditable'} contentEditable={editMode}>{User.postalCode}</p> </p>
+        <p className='profilePhoneNumber'>Phone Number: <p className={editMode ? 'editable' : 'noteditable'} contentEditable={editMode}>{User.phoneNumber}</p> </p>
+        <p className='profileEmail'>Email Adress: <p className={editMode ? 'editable' : 'noteditable'} contentEditable={editMode}> {User.email}</p></p>
+        <div className='profileButtons'>
+          <input className='profileEditButton' type='button' value='Edit' onClick={() => setEditMode(!editMode)}/>
+          <input className='profileLogOut' type='button' value='Log Out' onClick={handleLogout}/>
+        </div>
+      </div>
     </div> :
     <div><Login/></div>
   );
