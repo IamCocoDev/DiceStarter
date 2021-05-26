1a10cd549410c4ce36af235890d2fdcf06e05375
Angelo <angeloo.esc@gmail.com>
2021-05-26 16:25:24 -0300

    Firebase

---
+++ client/src/firebase-config.js
@@ -0,0 +1,9 @@
+export default {
+    apiKey: "AIzaSyBVvd8xEaiBdLHjLnePNyh8y8vIFDSCWBk",
+    authDomain: "dicestarter-32aec.firebaseapp.com",
+    projectId: "dicestarter-32aec",
+    storageBucket: "dicestarter-32aec.appspot.com",
+    messagingSenderId: "70853728681",
+    appId: "1:70853728681:web:95f8970a80c83d7bd14cb8",
+    measurementId: "G-0HBQNZT963"
+  };
