70aaaf18c7912ddb416144757d9533bea8e773c5
Xaldersoul <benjamin.g.guerra@hotmail.com>
2021-05-30 17:43:57 -0300

    añadido datos a la lista de usuarios

--- client/src/components/orderInfoProduct/orderInfoProduct.tsx
+++ client/src/components/orderInfoProduct/orderInfoProduct.tsx
@@ -3,12 +3,11 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 
 const OrderInfoProduct = (props) => {
-  const {name, picture, price} = props.product;
+  const {name, price} = props.product;
   const total = parseFloat(price) * props.product.productxorder.amount;
   return (
     <div>
       <h1>{name}</h1>
-      <img src={picture[0]} alt="" />
       <h1>Price: $ {price}</h1>
       <h1>Amount: {props.product.productxorder.amount}</h1>
       <h1>Sub total: $ {total}</h1>
